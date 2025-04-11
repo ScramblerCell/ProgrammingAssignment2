fb859ac579efa292572a3f8ab99a152548490eb3
Jonah Downing <jmd205@hotmail.com>
2025-04-11 00:42:46 -0500

    NB: BFS currently finds shortest distance, NOT nodes traversed;+header comments; +string constant FILE_TO_READ;modified DFS to be updateDistOfDFS;+updateDistOfBFS; modified trackDFS to trachSearchAlgo for use with both dfs and bfs; +while loop in main

--- PA2.cpp
+++ PA2.cpp
@@ -1,3 +1,10 @@
+/**
+ * @file PA2.cpp
+ * @author Jonah Downing
+ * @brief reads csv file FILE_TO_READ and creates statistic table using 
+ * @date 2025-04-11
+ */
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -5,13 +12,14 @@
 #include <map>
 #include <utility>
 #include <chrono>
-#include <thread>
 #include <iomanip>
+#include <queue>
 
 using namespace std;
 
 const char DELIMITER = ',';
 const int NUM_NODES = 26;
+const string FILE_TO_READ = "Test_Case_Assignment2.txt";
 
 
 vector<int> stripNonNums(const string& line) {
@@ -40,20 +48,49 @@ void TEST_printConnections(vector<int> adj[26]) {
         cout << endl;
     }
 }
-void DFS(int start, int target, vector<int> adj[], vector<bool>& isVisited, int& currDist) {
+void updateDistOfDFS(int start, int target, vector<int> adj[], vector<bool>& isVisited, int& currDist, bool& found) {
+    if (found) return;
+    
     //init start node as visited
     isVisited.at(start) = true;
-    if (start == target) return;
+    if (start == target) {
+        found = true;
+        return;
+    }   
     currDist++;
-
-
+    //depth first search 
     for (auto neighb : adj[start]) {//for each neighbor of start
         if (!isVisited.at(neighb)) {//if neighbor hasn't been visited
-            DFS(neighb, target, adj, isVisited, currDist);//do dfs on it
+            updateDistOfDFS(neighb, target, adj, isVisited, currDist, found);//do dfs on it
         }
     }
 }
-pair<int, double> trackDFS(int start, int target, vector<int> adj[]) {
+void updateDistOfBFS(int start, int target, vector<int> adj[], vector<bool>& isVisited, int& currDist) {
+    queue<pair<int, int> > bfsQueue; // pair<node, distance>
+    bfsQueue.push(make_pair(start, 0));
+    isVisited[start] = true;
+
+    while (!bfsQueue.empty()) {
+        int currNode = bfsQueue.front().first;
+        int currLevel = bfsQueue.front().second;
+        bfsQueue.pop();
+        //quit if target found
+        if (currNode == target) {
+            currDist = currLevel;
+            return;
+        }
+
+        for (int neighb : adj[currNode]) {
+            if (!isVisited[neighb]) {
+                isVisited[neighb] = true;
+                bfsQueue.push(make_pair(neighb, currLevel + 1));
+            }
+        }
+    }
+
+    currDist = -1; // if unreachable
+}
+pair<int, double> trackSearchAlgo(bool isDFS,int start, int target, vector<int> adj[]) {
     int currDist = 0;
     double currTime_ms = 0;
     //init visited vector
@@ -62,8 +99,10 @@ pair<int, double> trackDFS(int start, int target, vector<int> adj[]) {
     //start timer
     auto start_time = std::chrono::high_resolution_clock::now();
     
-    //run dfs to get distance
-    DFS(start, target, adj, isVisited, currDist);
+    //run search algo to get distance
+    bool found = false;
+    if (isDFS) updateDistOfDFS(start, target, adj, isVisited, currDist, found);
+    else updateDistOfBFS(start, target, adj, isVisited, currDist);
     
     //end timeer
     auto current_time = std::chrono::high_resolution_clock::now();
@@ -76,6 +115,8 @@ pair<int, double> trackDFS(int start, int target, vector<int> adj[]) {
 
     return trackedData;
 }
+
+
 //print functions
 void printLine() {
     cout << setfill('-') << setw(78) << "" << setfill(' ') << "" << endl;
@@ -89,10 +130,12 @@ void printHeader() {
 void printRun(int row, int DFS_dist, double DFS_time, int BFS_dist, double BFS_time) {
     cout << right << setw(8) << "N_0" << setw(8) << "N_"+to_string(row) << setw(15) << BFS_dist << setw(15) << BFS_time << setw(15) << DFS_dist <<  setw(15) << DFS_time << endl;
 }
+
+
 int main() {
     string testLine;
     vector<int> strippedInts;
-    ifstream csvFile("Test_Case_Assignment2.txt");
+    ifstream csvFile(FILE_TO_READ);
     vector<int> adj[NUM_NODES]; 
 
     //read and write csvFile into graph
@@ -103,16 +146,11 @@ int main() {
     printHeader();
     //do dfs from 0 to 9 for testing
     for (int i = 1; i < NUM_NODES-1; i++) {
-        pair<int, double> DFS_data = trackDFS(0, i, adj);
-
-        printRun(i, DFS_data.first, DFS_data.second, 0, 0);
+        pair<int, double> DFS_data = trackSearchAlgo(true, 0, i, adj);
+        pair<int, double> BFS_data = trackSearchAlgo(false, 0, i, adj);
+        printRun(i, DFS_data.first, DFS_data.second, BFS_data.first, BFS_data.second);
     }
     printLine();
-    
-    //cout << "took " << DFS_data.second << "ns long, traversing " << DFS_data.first << " nodes" << endl;
-    //perform BFS starting at N0 to N#
-
-
 
     //TEST_printConnections(adj);
 
